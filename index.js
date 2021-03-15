e87c21b53b5af455e2f1d9c8259433083fe5fb2a
Luca <luca.milanese90@gmail.com>
2021-03-15 22:39:13 +0100

    modified text string

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const myInformation = require('./information.js');
 
 
 console.log(cowsay.say({
-    text: `Hello I\'m ${myInformation.name} from ${myInformation.campus} Campus!`,
+    text: `Hello I'm ${myInformation.name} from ${myInformation.campus} Campus!`,
     e: "oO",
     T: "U "
 }));
