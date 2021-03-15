bc93a2462281e0817eea1ca0abb7087d4f81e999
Luca <luca.milanese90@gmail.com>
2021-03-15 22:25:42 +0100

    created object and cow say command

---
+++ index.js
@@ -0,0 +1,9 @@
+const cowsay = require("cowsay");
+const myInformation = require('./information.js');
+
+
+console.log(cowsay.say({
+    text: `Hello I\'m ${myInformation.name} from ${myInformation.campus} Campus!`,
+    e: "oO",
+    T: "U "
+}));
---
+++ information.js
@@ -0,0 +1,6 @@
+const person = {
+    name: 'Luca Milanese',
+    campus: 'Berlin'
+};
+
+module.exports = person;
